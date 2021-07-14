e7f57c6aac4138e40b6a3dcb8b983a73f17f8d9a
Yeison1994s <yeisonestivenlopezimbachi@gmail.com>
2021-07-14 09:07:12 -0500

    Repaso Base Datos OneToOne Srping Boot

---
+++ src/test/java/com/data/mariadb/MariaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
